10bceca6e9d0d704a3a999c4f4d9e10d05c59ef4
sarah_villegas <sarahvillegas95@gmail.com>
2017-03-30 04:07:20 +0000

    Create cart holding the price items and the total with shipping

--- Cart.php
+++ Cart.php
@@ -1,23 +1,70 @@
 <?php
 
+    $servername = getenv('IP');
+    $dbPort = 3306;
+    $database = "Techni"; 
+    $username = getenv('C9_USER');
+    $password = "";
+    $dbConn = new PDO("mysql:host=$servername;port=$dbPort;dbname=$database", $username, $password);
+    $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+
+    $grandTotal = 0; 
+
     //add to cart button is pressed
-    if(isset($_POST["addChart"])){
+    if(!isset($_POST["addChart"])){
         
         $_SESSION["cart"] = array(); 
+        $_SESSION["total"] = array();
     }
     if(isset($_POST["addCart"])){
         // pushes in the products id 
         // $_POST["addChart"]  == productID
+        
+        $price = "SELECT price 
+                    FROM `Product`
+                    WHERE '" . $_POST["addCart"] . "' = productID"; 
+        
+        $priceState = $dbConn->prepare($price);
+        $priceState->execute();
+        $productPrice = $priceState->fetch();
         array_push($_SESSION["cart"], $_POST["addChart"]);
+        array_push($_SESSION["total"], $productPrice["price"]);
     }
-    
     // when the order button is pressed 
     if(isset($_POST["ordered"])){
         
         unset($_SESSION["cart"]);
     }
     
+    // orderID 
+    if(isset($_POST["ship"])){
+        
+        $charge = 0; 
+        
+        if($_POST["ship"] == 1){
+            
+            $charge = 10000000;
+            calcTotal($charge);
+        }
+        else{
+            
+            $charge = 60000000;
+            calcTotal($charge);
+        }
+    }
     
+    
+    function calcTotal($charge){
+        
+        global $grandTotal; 
+        
+        foreach($_SESSION["total"] as $key => $value){
+            
+            $grandTotal += $value; 
+        }
+        
+        $grandTotal += $charge; 
+    }
     
 
 ?>
--- SearchingTechni.php
+++ SearchingTechni.php
@@ -9,9 +9,7 @@
     $password = "";
     $dbConn = new PDO("mysql:host=$servername;port=$dbPort;dbname=$database", $username, $password);
     $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-
-
-
+    
 // Books
 // Prints
 // Lithographs
@@ -21,53 +19,43 @@
 // Sculpture
 // Mixed Media 
 
-    $narrorTable = $narrorDown; 
+   // $narrorTable = $narrorDown; 
 
-    $artWork = $_GET["artWork"];
+    $artWork = 3;
 
-    function findTypeTable($artWork, $dbConn){
+    //function findTypeTable($artWork, $dbConn){
         
-        $narrorDown = "SELECT ProductType.productType, Product.productName 
-                            FROM ProductType 
-                            RIGHT JOIN Product
-                            ON $artWork = Product.productTypeId 
-                            ORDER BY Product.productName"; 
-                            
-                            
+    $narrorDown = "select productName, typeName 
+    from `Product` 
+    join `Product Type` 
+    on `Product Type`.productTypeID = `Product`.productTypeID 
+    where `Product`.productTypeID = '" . $artWork ."' order by `Product`.productName"; 
+                        
         
-        
-        // returns the new narrored list of 
-        return $narrorDown; 
-        
-    }
-    
-    
-    $narror = findTypeTable($artWork, $dbConn);
-    $narState = $dbConn->prepare($narror);
+    $narState = $dbConn->prepare($narrorDown);
     $narState->execute();
     
     
-    echo "<div style = \" width: 500px; height: 600;\">"; 
-                echo "<table class= \"chart\">
-                        <tr class = \"header\">
-                        <th colspan = \"3\" class = \"header\">Types of Products</td>
-                        </tr>
-                        <tr class = \"header\">
-                            <th>Product Type</th>
-                            <th>Number Of Products</th>
-                        </tr>"; 
-                
-                // Don't return the indexed values, just the associative
-                while ($narrorw = $narState->fetch(PDO::FETCH_ASSOC)){
-                    // var_dump($whereInRow); 
-                    echo "<tr  colspan = \"2\">";
-                    
-                    echo "<td>" .   $narror['productType'] . "</td>" .
-                    "<td>" .  $narror['productName '] . "</td>";
-                        
-                    echo "</tr>"; 
-                }
+        
+    while ($na = $narState->fetch()){
+       
+        echo "<br/><br/> Product Name: ".$na['productName'] . "  Product Type: " . $na['typeName'] . "<br/>"; 
 
+    }
+    
+    if(isset($_POST["artist"])){
+        
+        
+    }
+    if(isset($_POST["style"])){
+        
+        
+    }
+    if(isset($_POST["range"])){
+        
+        
+    }
+                
     // function findYear($narrorTable){
         
     //     global $narrorTable; 
@@ -87,53 +75,5 @@
         
     //     global $narrorTable; 
     // }
-
-    
-
-/*
-Demo of how to search 
-
-$whereSql = "SELECT ProductType.productType, COUNT(Product.productTypeId) AS 'numOfProducts' 
-                            FROM Product
-                            LEFT JOIN ProductType
-                            ON ProductType.productTypeId = Product.productTypeId 
-                            GROUP BY ProductType.productType";
-                            
-        
-                echo "<br /><br />Filtered SQL: $whereSql<br /><br />";
-                
-                // The prepare caches the SQL statement for N number of parameters imploded above
-                $whereStmt = $dbConn->prepare($whereSql);
-                
-                // Execute with the parameters. An interesting addition to this example: what 
-                // about if you are filtering (executing) on multiple parameters and you have 
-                // an IN clause??? Try it out!
-                $whereStmt->execute();
-                
-                echo "<div style = \" width: 500px; height: 600;\">"; 
-                echo "<table class= \"chart\">
-                        <tr class = \"header\">
-                        <th colspan = \"3\" class = \"header\">Types of Products</td>
-                        </tr>
-                        <tr class = \"header\">
-                            <th>Product Type</th>
-                            <th>Number Of Products</th>
-                        </tr>"; 
-                
-                // Don't return the indexed values, just the associative
-                while ($whereRow = $whereStmt->fetch(PDO::FETCH_ASSOC)){
-                    // var_dump($whereInRow); 
-                    echo "<tr  colspan = \"2\">";
-                    
-                    echo "<td>" .  $whereRow['productType'] . "</td>" .
-                    "<td>" . $whereRow['numOfProducts'] . "</td>";
-                        
-                    echo "</tr>"; 
-                }
-                
-                echo "</table></div>"; 
-            }// end of countProducts
-
-*/
 
 ?>
