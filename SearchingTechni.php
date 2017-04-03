3ed1ecc41dacb055cc0ac65b15bb80f9a90a0009
sarah_villegas <sarahvillegas95@gmail.com>
2017-04-03 20:57:41 +0000

    finish conditions for sorting the artwork based on bars

--- SearchingTechni.php
+++ SearchingTechni.php
@@ -10,97 +10,266 @@
     $dbConn = new PDO("mysql:host=$servername;port=$dbPort;dbname=$database", $username, $password);
     $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
     
-// Books
-// Prints
-// Lithographs
-// Original Artwork
-// Gift Cards
-// Photography 
-// Sculpture
-// Mixed Media 
-
-<<<<<<< HEAD
-   // $narrorTable = $narrorDown; 
-=======
-    $narrowTable = $narrowDown; 
->>>>>>> a139bdc2d00b20f095735d8ae26dd3ed5123511c
-
     $artWork = 3;
 
-<<<<<<< HEAD
-    //function findTypeTable($artWork, $dbConn){
-        
-    $narrorDown = "select productName, typeName 
-    from `Product` 
-    join `Product Type` 
-    on `Product Type`.productTypeID = `Product`.productTypeID 
-    where `Product`.productTypeID = '" . $artWork ."' order by `Product`.productName"; 
-                        
-        
-    $narState = $dbConn->prepare($narrorDown);
-    $narState->execute();
+    $productImgs = array(1 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/1.jpg\"></img></div>",
+                        2 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/2.jpg\"></img></div>", 
+                        3 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/3.jpg\"></img></div>", 
+                        4 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/4.jpg\"></img></div>", 
+                        5 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/5.jpg\"></img></div>",
+                        6 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/6.jpg\"></img></div>",
+                        7 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/7.png\"></img></div>",
+                        8 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/8.jpg\"></img></div>",
+                        9 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/9.jpg\"></img></div>",
+                        10 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/10.jpg\"></img></div>", 
+                        11 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/11.jpg\"></img></div>",
+                        12 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/12.jpg\"></img></div>", 
+                        13 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/13.jpg\"></img></div>",
+                        14 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/14.jpg\"></img></div>", 
+                        15 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/15.jpg\"></img></div>",
+                        16 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/16.jpg\"></img></div>", 
+                        17 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/17.jpg\"></img></div>", 
+                        18 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/18.jpg\"></img></div>", 
+                        19 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/19.jpg\"></img></div>", 
+                        20 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/20.jpg\"></img></div>", 
+                        21 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/busca.png\"></img></div>",
+                        22 => "<div id=\"slider2\"><img style= \"width:100%; height = 100%;\" src=\"images/gift.png\"></img></div>"); 
     
-    
+    if(isset($_POST["artist"]) && isset($_POST["style"]) && isset($_POST["range"])){
         
-    while ($na = $narState->fetch()){
-       
-        echo "<br/><br/> Product Name: ".$na['productName'] . "  Product Type: " . $na['typeName'] . "<br/>"; 
-
-    }
-    
-    if(isset($_POST["artist"])){
-        
-        
-    }
-    if(isset($_POST["style"])){
-        
-        
-    }
-    if(isset($_POST["range"])){
-        
-        
-    }
-                
-=======
-    function findTypeTable($artWork, $dbConn){
-        
-        $artwork = 3;
-        $artwork = intval($artwork); //must explicitly typecast to int to use number variable in SQL statement
-        
-        //for string variables do this:
-        // $type     = 'testing';
-        // $type     = mysql_real_escape_string($type);
-        
-        $narrowDown = "SELECT Product.productName, `Product Type`.typeName 
+        $narrowDown = "SELECT Product.productID
                        FROM Product 
                        INNER JOIN `Product Type` 
                        ON `Product Type`.productTypeID = Product.productTypeID 
-                       WHERE Product.productTypeID = $artwork
-                       ORDER BY `Product`.productName";
+                       WHERE Product.artist = '" . $_POST["artist"] . "' AND Product.price <= '" . $_POST["range"] . "' AND Product.style = '" . $_POST["style"] .
+                       "' ORDER BY `Product`.productName";
+        
+        
+        $narState = $dbConn->prepare($narrorDown);
+        $narState->execute();
+        
+        while ($na = $narState->fetch()){
+            
+            foreach(  $productImg as $key => $value){
+                
+                if($na['productID'] == $key){
+                    
+                    echo $value; 
+                    
+                }
+                
+            }// end of foreach 
 
-        // returns the new narrowed down list of art
-        return $narrowDown; 
+        }// end of while 
+    }
+    else if(isset($_POST["artist"]) && isset($_POST["style"])){
+        
+        $narrowDown = "SELECT Product.productID
+                       FROM Product 
+                       INNER JOIN `Product Type` 
+                       ON `Product Type`.productTypeID = Product.productTypeID 
+                       WHERE Product.artist = '" . $_POST["artist"] . "' AND Product.style = '" . $_POST["style"] . 
+                       "' ORDER BY `Product`.productName";
+        
+        
+        $narState = $dbConn->prepare($narrorDown);
+        $narState->execute();
+        
+        while ($na = $narState->fetch()){
+            
+            foreach(  $productImg as $key => $value){
+                
+                if($na['productID'] == $key){
+                    
+                    echo $value; 
+                    
+                }
+                
+            }// end of foreach 
+
+        }// end of while 
         
     }
+    else if(isset($_POST["artist"]) && isset($_POST["range"])){
+        
+        $narrowDown = "SELECT Product.productID
+                       FROM Product 
+                       INNER JOIN `Product Type` 
+                       ON `Product Type`.productTypeID = Product.productTypeID 
+                       WHERE Product.artist = '" . $_POST["artist"] . "' AND Product.price <= '" . $_POST["range"] . 
+                       "' ORDER BY `Product`.productName";
+        
+        
+        $narState = $dbConn->prepare($narrorDown);
+        $narState->execute();
+        
+        while ($na = $narState->fetch()){
+            
+            foreach(  $productImg as $key => $value){
+                
+                if($na['productID'] == $key){
+                    
+                    echo $value; 
+                    
+                }
+                
+            }// end of foreach 
+
+        }// end of while 
+    }
+    else if(isset($_POST["style"]) && isset($_POST["range"])){
+        
+        $narrowDown = "SELECT Product.productID
+                       FROM Product 
+                       INNER JOIN `Product Type` 
+                       ON `Product Type`.productTypeID = Product.productTypeID 
+                       WHERE Product.artist = '" . $_POST["style"] . "' AND Product.price <= '" . $_POST["range"] . 
+                       "' ORDER BY `Product`.productName";
+        
+        
+        $narState = $dbConn->prepare($narrorDown);
+        $narState->execute();
+        
+        while ($na = $narState->fetch()){
+            
+            foreach(  $productImg as $key => $value){
+                
+                if($na['productID'] == $key){
+                    
+                    echo $value; 
+                    
+                }
+                
+            }// end of foreach 
+
+        }// end of while 
+        
+    }
+    else if(isset($_POST["artist"])){
+        
+        
+        $narrowDown = "SELECT Product.productID
+                       FROM Product 
+                       INNER JOIN `Product Type` 
+                       ON `Product Type`.productTypeID = Product.productTypeID 
+                       WHERE Product.artist = " . $_POST["artist"] . 
+                       "ORDER BY `Product`.productName";
+        
+        
+        $narState = $dbConn->prepare($narrorDown);
+        $narState->execute();
+        
+        while ($na = $narState->fetch()){
+            
+            foreach(  $productImg as $key => $value){
+                
+                if($na['productID'] == $key){
+                    
+                    echo $value; 
+                    
+                }
+                
+            }// end of foreach 
+
+        }// end of while 
+    
+    }// end of if 
+    else if(isset($_POST["style"])){
+        
+        $narrowDown = "SELECT Product.productID
+                       FROM Product 
+                       INNER JOIN `Product Type` 
+                       ON `Product Type`.productTypeID = Product.productTypeID 
+                       WHERE Product.style = " . $_POST["style"] . 
+                       "ORDER BY `Product`.productName";
+        
+        
+        $narState = $dbConn->prepare($narrorDown);
+        $narState->execute();
+        
+        while ($na = $narState->fetch()){
+            
+            foreach(  $productImg as $key => $value){
+                
+                if($na['productID'] == $key){
+                    
+                    echo $value; 
+                    
+                }
+                
+            }// end of foreach 
+
+        }// end of while 
+        
+    }
+    else if(isset($_POST["range"])){
+        
+        $narrowDown = "SELECT Product.productID
+                       FROM Product 
+                       INNER JOIN `Product Type` 
+                       ON `Product Type`.productTypeID = Product.productTypeID 
+                       WHERE Product.price = " . $_POST["range"] . 
+                       "ORDER BY `Product`.productName";
+        
+        
+        $narState = $dbConn->prepare($narrorDown);
+        $narState->execute();
+        
+        while ($na = $narState->fetch()){
+            
+            foreach(  $productImg as $key => $value){
+                
+                if($na['productID'] == $key){
+                    
+                    echo $value; 
+                    
+                }
+                
+            }// end of foreach 
+
+        }// end of while 
+    }
+                
+
+    // function findTypeTable($artWork, $dbConn){
+        
+    //     $artwork = 3;
+    //     $artwork = intval($artwork); //must explicitly typecast to int to use number variable in SQL statement
+        
+    //     //for string variables do this:
+    //     // $type     = 'testing';
+    //     // $type     = mysql_real_escape_string($type);
+        
+    //     $narrowDown = "SELECT Product.productID
+    //                   FROM Product 
+    //                   INNER JOIN `Product Type` 
+    //                   ON `Product Type`.productTypeID = Product.productTypeID 
+    //                   WHERE Product.productTypeID = $artwork
+    //                   ORDER BY `Product`.productName";
+
+    //     // returns the new narrowed down list of art
+    //     return $narrowDown; 
+        
+    // }
     
     
-    $narrow = findTypeTable($artWork, $dbConn);
-    $statement = $dbConn->prepare($narrow);
-    $statement->execute();
+    // $narrow = findTypeTable($artWork, $dbConn);
+    // $statement = $dbConn->prepare($narrow);
+    // $statement->execute();
     
     
-    echo "<table>";
-            echo "<tr>" . "<td>" . "TYPE" . "</td>" . "<td>" . "PRODUCT" . "</td>" . "</tr>";
-            while ($row = $statement->fetch(PDO::FETCH_ASSOC)) 
-            {
-                echo "<tr>";
-                    echo "<td>" . $row["typeName"] . "</td>";
-                    echo "<td>" . $row["productName"] . "</td>";
-                echo "</tr>";
-            }
-    echo "</table>";
+    // echo "<table>";
+    //         echo "<tr>" . "<td>" . "TYPE" . "</td>" . "<td>" . "PRODUCT" . "</td>" . "</tr>";
+    //         while ($row = $statement->fetch(PDO::FETCH_ASSOC)) 
+    //         {
+    //             echo "<tr>";
+    //                 echo "<td>" . $row["typeName"] . "</td>";
+    //                 echo "<td>" . $row["productName"] . "</td>";
+    //             echo "</tr>";
+    //         }
+    // echo "</table>";
     
->>>>>>> a139bdc2d00b20f095735d8ae26dd3ed5123511c
+
     // function findYear($narrorTable){
         
     //     global $narrorTable; 
